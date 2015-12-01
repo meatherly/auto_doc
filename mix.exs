b8cc164b5bd5ccbcc8098655c4e3dc800547ad17
Michael Eatherly <michael.eatherly@metova.com>
2015-12-01 01:26:00 -0600

    adding info for hex package

--- mix.exs
+++ mix.exs
@@ -10,29 +10,18 @@ defmodule AutoDoc.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      name: "AutoDoc",
+     package: package,
      source_url: "https://github.com/meatherly/auto_doc",
      deps: deps,
      docs: [extras: ["README.md"]],
      elixirc_paths: elixirc_paths(Mix.env)]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :plug],
       mod: {AutoDoc, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:plug, "~> 1.0"},
     {:poison, "~> 1.5"},
@@ -44,4 +33,17 @@ defmodule AutoDoc.Mixfile do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp description do
+  """
+  A package that will create REST API docs based on your ExUnit tests. 
+  """
+  end
+
+  defp package do
+      [files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Michael Eatherly"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/meatherly/auto_doc"}]
+  end
 end
