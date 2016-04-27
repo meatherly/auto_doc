eb30240a2687cc85b404ca66c8c9e9619a9df601
Hugues Dubois <hug@h13d.local>
2016-04-27 18:47:23 +0200

    Fix tests

--- test/auto_doc/agent_test.exs
+++ test/auto_doc/agent_test.exs
@@ -4,16 +4,16 @@ defmodule AutoDoc.AgentTest do
   # alias AutoDoc.Agent
 
   test "It should not generate api docs file if exit code is 1" do
-    capture_io(fn ->
+    assert capture_io(fn ->
       AutoDoc.Agent.write_file(1)
-    end) == "Tests have failed. No docs will be generated"
+    end) == "Tests have failed. No docs will be generated\n"
   end
 
 
   test "It should generate api docs file if the exit code is 0" do
-    capture_io(fn ->
+    assert capture_io(fn ->
       AutoDoc.Agent.write_file(0)
       assert File.exists?("#{Path.expand(".")}/api_docs.html")
-    end) == "Tests have failed. No docs will be generated"
+    end) == "Api docs have been created.\n"
   end
 end
