32ff27a941d16ed4ffeb881cbf6e2e57eab3bfa0
Hugues Dubois <hug@h13d.local>
2016-04-28 10:32:07 +0200

    Add markdown support

--- lib/auto_doc/agent.ex
+++ lib/auto_doc/agent.ex
@@ -30,21 +30,26 @@ defmodule AutoDoc.Agent do
   @doc false
   def write_file(exit_code) do
     exit_code
-      |> write_file("#{Path.expand(".")}/api_docs.html")
+      |> write_file("#{Path.expand(".")}/api_docs")
   end
 
   @doc false
   def write_file(exit_code, file_name) do
+    write_file(exit_code, file_name, "md")
+  end
+
+  @doc false
+  def write_file(exit_code, file_name, tpl_format) do
     case exit_code do
       1 ->
         Mix.Shell.IO.info("Tests have failed. No docs will be generated")
       0 ->
         tests = Agent.get(__MODULE__, fn(docs) -> docs  end)
         file_contents =
-          Path.join([__DIR__, "..", "templates/api_docs.html.eex"])
+          Path.join([__DIR__, "..", "templates/api_docs.#{tpl_format}.eex"])
           |> Path.expand
           |> EEx.eval_file([tests: tests])
-        File.write!(file_name, file_contents)
+        File.write!("#{file_name}.#{tpl_format}", file_contents)
         Mix.Shell.IO.info("Api docs have been created.")
       _ ->
         Mix.Shell.IO.info("Something Bad has happened. No docs have been generated.")
